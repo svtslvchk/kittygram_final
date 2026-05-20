b80caac9db0e9f6c33aa52aa4a57efe417283205
svtslvchk <solovevsr@gmail.com>
2026-05-20 19:18:09 +0300

    fix tests

--- backend/cats/tests.py
+++ backend/cats/tests.py
@@ -1,5 +1,4 @@
 from http import HTTPStatus
-
 from django.contrib.auth import get_user_model
 from django.test import TestCase
 from rest_framework.test import APIClient
@@ -7,8 +6,8 @@ from rest_framework.test import APIClient
 
 class CatsAPITestCase(TestCase):
     def setUp(self):
-        User = get_user_model()
-        self.user = User.objects.create_user(username='auth_user')
+        user = get_user_model()
+        self.user = user.objects.create_user(username='auth_user')
         self.client = APIClient()
         self.client.force_authenticate(user=self.user)
 
